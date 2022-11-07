e64f57b0a91096b3ef75148aab6582c00e55f8c7
catpea <corporate.miou@gmail.com>
2022-11-07 08:10:11 -0500

    New Release

--- alerts.js
+++ alerts.js
@@ -1,7 +1,7 @@
 export default async function(){
 
   const response = [
-    {type: 'success', urgent:false, text:`100 Hour (4.3GB mp3) Version Uploaded To The Internet Archive`, link:{title:`Download the 4GB mp3 at Archive.org`, url:'https://archive.org/details/audiobook_202211'}, note:'Every once in a while I smush all the files and upload them to the Internet Archive as a kind of an Audio Book'},
+    {type: 'warning', urgent:true, text:`100 Hour (4.3GB mp3) Version Uploaded To The Internet Archive`, link:{title:`Download the 4.3GB mp3 at Archive.org`, url:'https://archive.org/details/audiobook_202211'}, note:'Every once in a while I smush all the files and upload them to the Internet Archive as a kind of an Audio Book'},
     {type: 'info', urgent:false,  text:`I added "Lectures" to the website menu, it contains the Westalnd Warrior video lectures. You can sort it by latest (default) or by the intended order.`, link:{ title: `Lectures`, url:'/lectures.html'}, note:``},
     {type: 'info', urgent:false,  text:`Found a bug, I have to write a content integrity checker, some posts show markdown source, becasue it was escaped somewhere along the recent upgrades.`, note:`I will have to write a post-integrity-checker that will carefully test all the posts for a range of problems.`},
     // {type: 'warning', urgent:true,  html:`This is the new website, I am still searching for bugs, but it all seems OK.`},
