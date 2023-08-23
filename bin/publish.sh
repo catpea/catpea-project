623cf767d55504627b5fea60d0046b9db3243cb2
catpea <corporate.miou@gmail.com>
2023-08-23 00:08:54 -0400

    New Release

--- bin/publish.sh
+++ bin/publish.sh
@@ -1,22 +1,15 @@
 #!/bin/bash
 
-echo
-echo
-echo GITHUB/chapters updating chapter 07, if you are working on chapter 08, update this build file!
-echo you must cd and push 08 now, not 07;
-echo NOTE: antwerp will generate into "dist/chapters/chapter-07" but to set it up:
-echo       you need to create a github repo first, then clone it and then run build.
-echo       REMEMBER to setup pages branch "main" folder "/docs"
+echo -ne "\n\n\n\n\n\n\n\n\n\n"
+echo "1/4 - GITHUB/chapters updating chapter 07 -----------------------------------------------------------------------------------------------------"
 cd dist/chapters/chapter-07;
 git add .;
 git commit -m "Chapter content updates.";
 git push;
 cd -;
 
-
-echo
-echo
-echo GITHUB: updating catpea.github.io
+echo -ne "\n\n"
+echo "2/4 - GITHUB: updating catpea.github.io -------------------------------------------------------------------------------------------------------"
 cd $(antwerp read catpea-github configuration.dest)
 git add .;
 git commit -m "Blog updates";
@@ -29,15 +22,19 @@ cd -;
 # echo GANDI/lftp: updating catpea.github.io
 # lftp -f $(antwerp read catpea-gandi configuration.home)/gandi-hosting-batchfile.lftp;
 
-
-echo
-echo
-echo GITHUB: saving MAIN DATABASE: catpea-project repository
+echo -ne "\n\n"
+echo "3/4 - GITHUB: saving MAIN DATABASE: catpea-project repository ---------------------------------------------------------------------------------"
 npm run save;
 
-
-echo
-echo "CONTENT PUBLISHED"
+echo -ne "\n\n"
+echo "4/4 - END: CONTENT PUBLISHED ------------------------------------------------------------------------------------------------------------------"
 echo reminders:
+echo -ne "\n\n"
+echo If you are working on chapter 08, update this build file!
+echo you must cd and push 08 now, not 07;
+echo NOTE: antwerp will generate into "dist/chapters/chapter-07" but to set it up:
+echo       you need to create a github repo first, then clone it and then run build.
+echo       REMEMBER to setup pages branch "main" folder "/docs"
+
 echo github website location: $(antwerp read catpea-github configuration.dest)
 # echo gandi website location: $(antwerp read catpea-gandi configuration.dest)
